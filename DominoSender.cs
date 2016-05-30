da0d83834c63479bbf5dc950be2d5bca9bdbbd81
Aliaksei-Karaliou <alexeykoroliov@gmail.com>
2016-05-30 22:23:54 +0300

    Server Created 2

---
+++ DominoSender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominoCourseWork
+{
+    [Serializable]
+    public class DominoSender
+    {
+        public Domino Domino { get; set; }
+        public DominoSendingState State { get; set; }
+        public DominoSender(Domino Domino, DominoSendingState State)
+        {
+            this.Domino = Domino;
+            this.State = State;
+        }
+        public string Serialize()
+        {
+            XMLSerializer<DominoSender> xml = new XMLSerializer<DominoSender>();
+            return xml.Write(this);
+        }
+    }
+}
